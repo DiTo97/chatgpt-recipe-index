e3e3faf2ee4a9cf7db1835eefdcbf52271024b7a
f.minutoli <f.minutoli@reply.it>
2023-01-06 00:12:43 +0100

    Travel setup `recipe-database`

--- src/recipe-database/package/recipe_database/__init__.py
+++ src/recipe-database/package/recipe_database/__init__.py
@@ -2,7 +2,4 @@ import pathlib
 
 
 ROOT = pathlib.Path(__file__).parent.resolve()
-"""The Python package root"""
-
-DIRPATH_resources = ROOT / "resources"
-"""The Python package resources dirpath"""
+"""The package root"""
--- src/recipe-database/package/setup.py
+++ src/recipe-database/package/setup.py
@@ -1,11 +1,13 @@
 import pathlib
+import typing
+
 from setuptools import find_packages, setup
 
 
 _ROOT = pathlib.Path(__file__).parent.resolve()
 
 
-def _find_requirements() -> list[str]:
+def _find_requirements() -> typing.List[str]:
     """It finds Python requirements in 'requirements.txt'"""
     filepath = _ROOT / "requirements.txt"
 
@@ -13,28 +15,17 @@ def _find_requirements() -> list[str]:
         return f.read().splitlines()
 
 
-def _find_resources_paths() -> list[str]:
-    """It finds resources paths under 'recipe_database/resources/'"""
-    dirname = "recipe_database"
-    dirpath = _ROOT / dirname / "resources"
-
-    return [str(r.relative_to(dirpath)) for r in dirpath.rglob("*")]
-
-
-def main():
-    setup(**{
-        "author": "Federico Minutoli",
-        "author_email": "fede97.minutoli@gmail.com",
-        "description": "A common library of data and ORM models of the recipe index",
-        "install_requires": _find_requirements(),
-        "name": "recipe_database",
-        "packages": find_packages(),
-        "package_data": {
-            "recipe_database": _find_resources_paths(),
-            "tests": ["stubs/*"]
-        },
-        "version": "0.1.0",
-    })
+def main() -> None:
+    setup(
+        author="Federico Minutoli",
+        author_email="fede97.minutoli@gmail.com",
+        description="A common library of data and ORM models of the recipe index",
+        include_package_data=True,
+        install_requires=_find_requirements(),
+        name="recipe_database",
+        packages=find_packages(),
+        version="0.0.0",
+    )
 
 
 if __name__ == "__main__":

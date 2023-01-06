5cd6fefcf832eb2a904c6b74a39a762b36b111b0
f.minutoli <f.minutoli@reply.it>
2023-01-06 05:43:08 +0100

    Add CRUD-variations of datamodels

--- src/recipe-database/package/recipe_database/datamodels.py
+++ src/recipe-database/package/recipe_database/datamodels.py
@@ -3,15 +3,29 @@ import typing
 from sqlmodel import Field, Relationship, SQLModel
 
 
-class Dosage(SQLModel, table=True):
-    """A SQL model that maps the dosage of any ingredient in any recipe"""
+class Dosage_base(SQLModel):
+    """A base SQL model that maps the dosage of any ingredient in any recipe"""
 
-    recipe_id: typing.Optional[int] = Field(
-        default=None,
-        description="The unique Id of the recipe",
-        foreign_key="recipe.id",
-        primary_key=True,
-    )
+    quantity: typing.Optional[float] = Field(default=None, gt=0.0)
+    unit_of_measure: typing.Optional[str] = None
+
+    ingredient: "Ingredient" = Relationship(back_populates="dosages")
+    recipe: "Recipe" = Relationship(back_populates="dosages")
+
+
+class Dosage_create(Dosage_base):
+    """A create SQL model that maps the dosage of any ingredient in any recipe"""
+
+
+class Dosage_read(Dosage_base):
+    """A read SQL model that maps the dosage of any ingredient in any recipe"""
+
+    ingredient_id: int
+    recipe_id: int
+
+
+class Dosage(Dosage_base, table=True):
+    """A SQL model that maps the dosage of any ingredient in any recipe"""
 
     ingredient_id: typing.Optional[int] = Field(
         default=None,
@@ -20,16 +34,41 @@ class Dosage(SQLModel, table=True):
         primary_key=True,
     )
 
-    quantity: typing.Optional[str] = None
+    recipe_id: typing.Optional[int] = Field(
+        default=None,
+        description="The unique Id of the recipe",
+        foreign_key="recipe.id",
+        primary_key=True,
+    )
 
-    recipe: "Recipe" = Relationship(back_populates="dosages")
-    ingredient: "Ingredient" = Relationship(back_populates="dosages")
+
+class Ingredient_base(SQLModel):
+    """A base SQL model that characterizes an ingredient"""
+
+    name: str = Field(index=True, unique=True)
+
+    dosages: typing.List[Dosage] = Relationship(back_populates="ingredient")
 
 
-class Recipe(SQLModel, table=True):
-    """A SQL model that characterizes a recipe"""
+class Ingredient_create(Ingredient_base):
+    """A create SQL model that characterizes an ingredient"""
+
+
+class Ingredient_read(Ingredient_base):
+    """A read SQL model that characterizes an ingredient"""
+
+    id: int
+
+
+class Ingredient(Ingredient_base, table=True):
+    """A SQL model that characterizes an ingredient"""
 
     id: typing.Optional[int] = Field(default=None, primary_key=True)
+
+
+class Recipe_base(SQLModel):
+    """A base SQL model that characterizes a recipe"""
+
     name: str = Field(index=True, unique=True)
     type: str = Field(index=True)
     preparation: str
@@ -39,10 +78,17 @@ class Recipe(SQLModel, table=True):
     dosages: typing.List[Dosage] = Relationship(back_populates="recipe")
 
 
-class Ingredient(SQLModel, table=True):
-    """A SQL model that characterizes an ingredient"""
+class Recipe_create(Recipe_base):
+    """A create SQL model that characterizes a recipe"""
+
+
+class Recipe_read(Recipe_base):
+    """A read SQL model that characterizes a recipe"""
+
+    id: int
+
+
+class Recipe(Recipe_base, table=True):
+    """A SQL model that characterizes a recipe"""
 
     id: typing.Optional[int] = Field(default=None, primary_key=True)
-    name: str = Field(index=True, unique=True)
-
-    dosages: typing.List[Dosage] = Relationship(back_populates="ingredient")
